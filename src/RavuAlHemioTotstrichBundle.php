46f7d4ea752d0a5de1cde4a14bc694af7a4ccb9c
Ondřej Hošek <ondra.hosek@gmail.com>
2016-10-19 22:49:02 +0200

    Bundle: return the correct container extension

--- src/RavuAlHemioTotstrichBundle.php
+++ src/RavuAlHemioTotstrichBundle.php
@@ -2,8 +2,13 @@
 
 namespace RavuAlHemio\TotstrichBundle;
 
+use RavuAlHemio\TotstrichBundle\DependencyInjection\TotstrichExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RavuAlHemioTotstrichBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new TotstrichExtension();
+    }
 }
